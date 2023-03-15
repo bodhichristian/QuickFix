91c7582e1728039fec3d396bba5ec2e811be46c7
bodhichristian <110639779+bodhichristian@users.noreply.github.com>
2023-03-14 21:02:11 -0400

    Added comments for DataController.swift

--- QuickFix/Models/DataController.swift
+++ QuickFix/Models/DataController.swift
@@ -8,35 +8,42 @@
 import CoreData
 
 class DataController: ObservableObject {
+    // The container that holds the Core Data stack
     let container: NSPersistentCloudKitContainer
     
+    // Published property that tracks the currently selected filter
     @Published var selectedFilter: Filter? = Filter.all
     
+    // A static instance of the DataController that can be used for previews
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
         dataController.createSampleData()
         return dataController
     }()
     
+    // Initializes the Core Data stack
     init(inMemory: Bool = false ) {
         container = NSPersistentCloudKitContainer(name: "Main")
         
+        // Configure the persistent store for in-memory usage
         if inMemory {
-            // If in memory, save nowhere
             container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
         }
         
+        // Load the persistent stores and handle any errors
         container.loadPersistentStores { storeDescription, error in
-            if let error {
+            if let error = error {
                 fatalError("Fatal error loading store: \(error.localizedDescription)")
             }
         }
     }
     
+    // Creates sample data and saves it to the persistent store
     func createSampleData() {
-        // Current pool of data in RAM
+        // Get the managed object context from the container
         let viewContext = container.viewContext
         
+        // Create some sample tags and issues
         for i in 1...5 {
             let tag = Tag(context: viewContext)
             tag.id = UUID()
@@ -52,48 +59,58 @@ class DataController: ObservableObject {
                 tag.addToIssues(issue)
             }
         }
-        // Write sample data to persistent storage
+        
+        // Save the sample data to the persistent store
         try? viewContext.save()
     }
     
+    // Saves changes to the managed object context
     func save() {
-        // Saves only if viewContext has been changed
+        // Only save if there are changes in the managed object context
         if container.viewContext.hasChanges {
             try? container.viewContext.save()
         }
     }
     
+    // Deletes a managed object from the managed object context
     func delete(_ object: NSManagedObject) {
+        // Notify observers that the object is about to be deleted
         objectWillChange.send()
+        
+        // Delete the object from the managed object context and save changes
         container.viewContext.delete(object)
         save()
     }
     
+    // Deletes all managed objects that match a fetch request
     private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
+        // Create a batch delete request with the specified fetch request
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-        // Receive Object IDs as result type
+        
+        // Specify that we want to receive object IDs as the result type
         batchDeleteRequest.resultType = .resultTypeObjectIDs
-
-        // Fetch request
-        // .execute() takes any fetch request
-        // Typecast as NSBatchDeleteResult
+        
+        // Execute the batch delete request and handle any errors
         if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
-            // Create a dictionary
-            // Typecast result as an array of NSManagedObjectID
-            // delete.result is of type Any?
+            // Create a dictionary that maps deleted objects to their object IDs
             let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
-            // Merge the changes to the view context
+            
+            // Merge the changes into the managed object context
             NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
         }
     }
     
+    // Deletes all tags and issues from the persistent store.
     func deleteAll() {
+        // Fetch all tags
         let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
         delete(request1)
         
+        // Fetch all issues
         let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
         delete(request2)
         
+        // Save changes
         save()
     }
 }
